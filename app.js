de1b739f352eca0dde4425d3fb3b4df670b3e938
Sara Maria <saramariasantosdev@gmail.com>
2025-07-24 18:48:37 -0300

    Atualiza a lógica de sorteamento e interação com botões

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
-function sortear(){
+function sortear() {
     let quantidade = parseInt(document.querySelector('#quantidade').value);
     let de = parseInt(document.querySelector('#de').value);
     let ate = parseInt(document.querySelector('#ate').value);
 
-    let numero = obterNumeroAleatorio(de, ate);
-    alert(`Número Aleatório ${numero}`);
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
+        sorteados.push(numero);
+    }
+
+    if (sorteados.length != 0) {
+        alterarStatusBotao();
+        document.querySelector('#resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+    } else {
+        document.querySelector('#resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>`;
+    }
 }
 
-function obterNumeroAleatorio(min, max){
-  return Math.floor(Math.random() * (max - min) + min);
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar')
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao-ativo');
+    } else {
+        botao.classList.remove('container__botao-ativo');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+function reiniciar() {
+    let quantidade = document.querySelector('#quantidade').value = '';
+    let de = document.querySelector('#de').value = '';
+    let ate = document.querySelector('#ate').value = '';
+
+    document.querySelector('#resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>`;
+
+    alterarStatusBotao();
 }
